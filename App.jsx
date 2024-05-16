e070082dcc9cd4c5a144a35a4af2ebbb0e1f7ef0
itsN4H33M <naheemsanju@gmail.com>
2024-05-17 00:01:22 +0530

    initial commit

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Login from './Screens/Login'
+import Register from './Screens/Register'
+import Home from './Screens/Home'
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
